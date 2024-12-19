8be8edb722c64b18ed12fcd135a196ed996635ac
mithon <mithonphilip@gmail.com>
2024-12-19 23:14:05 +0600

    second commit message here

---
+++ Lecture-03/app.js
@@ -0,0 +1,17 @@
+let a = myfun(10, 20);
+let b = myfun(20, 30);
+let c = myfun(30, 40);
+let d = myfun(40, 50);
+let e = myfun(50, 60);
+let f = myfun(60, 70);
+let g = myfun(70, 80);
+
+function myfun(a, b) {
+  return (a + b) * 2;
+}
+
+console.log(a, b, c, d, e, f, g);
+
+/**
+ * We have data, make a variable for it.
+ */
