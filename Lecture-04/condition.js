dca41388c94dd124910c6ca8de1d36c82bcf814c
mithon <mithonphilip@gmail.com>
2024-12-21 01:03:37 +0600

    learning condition compleated

--- Lecture-04/condition.js
+++ Lecture-04/condition.js
@@ -2,4 +2,47 @@
  * Condition
  * - Brain of a computer
  */
+if (studyBasic) {
+  wontJoin();
+}
 
+if (studyAdvanced) {
+  join();
+}
+
+if (techerSpeaks) {
+  silent();
+}
+
+if (!techerSpeaks) {
+  shout();
+}
+
+/**
+ *  Scenario 1 - single branch
+ * if condition
+ */
+if (hasMoney) {
+  purchasePhone();
+}
+
+/**
+ * Scenario 2 - Two branch
+ * if else condition
+ */
+if (toss === head) {
+  win();
+} else {
+  lose();
+}
+
+/**
+ * Scenario 3 - Multiple Branch
+ */
+if (1 > 1) {
+  big();
+} else if (1 < 1) {
+  small();
+} else {
+  same();
+}
