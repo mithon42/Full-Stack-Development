b996c1b04f2ef3f7a9cbc941bdadcdd1543af63d
mithon <mithonphilip@gmail.com>
2024-12-21 01:31:58 +0600

    learning loops compleated

--- Lecture-04/loops.js
+++ Lecture-04/loops.js
@@ -2,6 +2,10 @@ for (let index = 1; index <= 10; index++) {
   console.log(index, "Welcome");
 }
 
+for (let i = 1; i <= 100; i++) {
+  console.log(i, " Welcome mithon");
+}
+
 for (let i = 1; i <= 11; i++) {
   // it's a new js file,
   // we can write any valid js code here.
@@ -9,13 +13,24 @@ for (let i = 1; i <= 11; i++) {
   console.log(`${i} Welcomes`);
 }
 
-// There are total three types of loop available in JS
+/** There are total three types of loop available in JS
+ * 1. for (when we know the range)
+ * 2. while (when we don't know the range)
+ * 3. do while ***
+ */
+
+while (true) {
+  let namber = Math.ceil(Math.random() * 100);
+  console.log("random number", namber);
+
+  if (namber === 99) break;
+}
 
 while (true) {
   let nam = Math.ceil(Math.random() * 100);
   console.log("mithon", nam);
 
-  if (nam === 99) break;
+  if (nam === 55) break;
 }
 
 do {
