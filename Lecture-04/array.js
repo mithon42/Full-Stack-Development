5ea2eeeac520cbb2fedb9b82bce39e163ec7b5f9
mithon <mithonphilip@gmail.com>
2024-12-21 02:06:36 +0600

    learning Array compleated

--- Lecture-04/array.js
+++ Lecture-04/array.js
@@ -1,5 +1,18 @@
-const students = ['mithon', 'likhon', 'mim', 'joy', 'adu'];
+// const students = ['mithon', 'likhon', 'mim', 'joy', 'adu'];
 
-for (let i = 0; i < students.length; i++) {
-    console.log(students[i], students[i].toUpperCase());
+// for (let i = 0; i < students.length; i++) {
+//     console.log(students[i], students[i].toUpperCase());
+// }
+
+const AllStudentNames = [
+  "mithon",
+  "likhon",
+  "mim",
+  "jui",
+  "akhi",
+  "taslima",
+  "tuli",
+];
+for (let i = 0; i < AllStudentNames.length; i++) {
+  console.log(AllStudentNames[i], "->", AllStudentNames[i].toUpperCase());
 }
