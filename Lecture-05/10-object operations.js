4b9628b8f7efa348a8db2a3f73ee49edfcf8b2e9
mithon <mithonphilip@gmail.com>
2024-12-27 23:21:32 +0600

    object completed

---
+++ Lecture-05/10-object operations.js
@@ -0,0 +1,33 @@
+const microphone = {
+  brand: "Fifine",
+  indicator: true,
+  price: 8000,
+  color: "Black",
+  cableColor: "White",
+  startRecording() {
+    console.log("Recording Start");
+  },
+  stopRecording() {
+    console.log("Recording Stop");
+  },
+  // toString() {
+  //   return this.brand
+  // }
+};
+
+/**
+ * Teare are two different parts in Object
+ * 1. Noun -> Adjective (State/data/property/field)
+ * 2. Varb -> Functionlities (stast, stop)
+ */
+
+microphone.startRecording();
+console.log(microphone);
+
+console.log(microphone.toString());
+
+const testObj = new Object();
+testObj.name = "Test name object";
+testObj.time = new Date();
+console.log(testObj);
+console.log('My microphone = ' + microphone);
---
+++ Lecture-05/11-notation.js
@@ -0,0 +1,2 @@
+const empty = {};
+console.log(!empty);
